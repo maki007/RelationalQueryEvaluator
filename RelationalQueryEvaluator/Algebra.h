d7ac5c8474d94c32801a5bff802c2c6807c9a205
maki007 <maki007@gmail.com>
2013-10-28 01:41:41 +0100

    stated  implementing reading input xml

--- RelationalQueryEvaluator/Algebra.h
+++ RelationalQueryEvaluator/Algebra.h
@@ -15,6 +15,9 @@
 #include <xercesc/dom/DOMNodeList.hpp>
 #include <xercesc/dom/DOMText.hpp>
 
+#include <iostream>
+
+
 XERCES_CPP_NAMESPACE_USE
 
 #ifndef AlgebraHPP
@@ -27,17 +30,49 @@ public:
 	{
 	
 	}
+
 };
 
 class UnaryAlgebraNodeBase : public AlgebraNodeBase
 {
 public:
 	AlgebraNodeBase * child;
-	~UnaryAlgebraNodeBase()
+
+	UnaryAlgebraNodeBase(DOMElement * element)
 	{
-		
+
+
+		XMLCh *input = XMLString::transcode("input");
+
+		DOMNodeList * childs=element->getChildNodes();
+		for(XMLSize_t i=0;i<childs->getLength();++i)
+		{
+			if(childs->item(i)->getNodeType() == DOMElement::ELEMENT_NODE)
+			{
+				if(XMLString::compareString(childs->item(i)->getNodeName(), input)==0)
+				{
+					DOMNodeList * inputChildren = childs->item(i)->getChildNodes();
+					for(XMLSize_t j=0;j<inputChildren->getLength();++j)
+					{
+						if(inputChildren->item(j)->getNodeType() == DOMElement::ELEMENT_NODE)
+						{
+							const XMLCh * elementName = inputChildren->item(j)->getNodeName();
+							if(XMLString::compareString(elementName,XMLString::transcode("group"))==0)
+							{
+								std::cout << "ok" << std::endl;
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
+	~UnaryAlgebraNodeBase()
+	{
+		delete child;
+	}
+	
 };
 
 class BinaryAlgebraNodeBase : public AlgebraNodeBase
@@ -45,9 +80,16 @@ class BinaryAlgebraNodeBase : public AlgebraNodeBase
 public:
 	AlgebraNodeBase * leftChild;
 	AlgebraNodeBase * rightChild;
+
+	BinaryAlgebraNodeBase(DOMElement * element)
+	{
+	
+	}
+
 	~BinaryAlgebraNodeBase()
 	{
-		
+		delete leftChild;
+		delete rightChild;
 	}
 };
 
@@ -59,35 +101,55 @@ public:
 class Sort : public UnaryAlgebraNodeBase
 {
 public:
-	Sort(DOMElement * element)
+	Sort(DOMElement * element) : UnaryAlgebraNodeBase(element)
 	{
-	
+
 	}
 };
 
 class Group : public UnaryAlgebraNodeBase
 {
 public:
+	Group(DOMElement * element) : UnaryAlgebraNodeBase(element)
+	{
+		
+	}
 };
 
 class ColumnOperations: public UnaryAlgebraNodeBase
 {
 public:
+	ColumnOperations(DOMElement * element) : UnaryAlgebraNodeBase(element)
+	{
+		
+	}
 };
 
 class Join : public BinaryAlgebraNodeBase
 {
 public:
+	Join(DOMElement * element)  : BinaryAlgebraNodeBase(element)
+	{
+		
+	}
 };
 
 class AntiJoin : public BinaryAlgebraNodeBase
 {
 public:
+	AntiJoin(DOMElement * element) : BinaryAlgebraNodeBase(element)
+	{
+		
+	}
 };
 
 class Selection : public UnaryAlgebraNodeBase
 {
 public:
+	Selection(DOMElement * element) : UnaryAlgebraNodeBase(element)
+	{
+		
+	}
 };
 
 
