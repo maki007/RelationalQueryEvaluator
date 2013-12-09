e321fb48eb62a27ead17ecafbd8822b0488f14d2
maki007 <maki007@gmail.com>
2013-12-09 23:17:18 +0100

    finished semantic checker

--- RelationalQueryEvaluator/SemanticChecker.cpp
+++ RelationalQueryEvaluator/SemanticChecker.cpp
@@ -1,6 +1,8 @@
 #include "AlgebraVisitors.h"
 #include <set>
 #include "ExpressionVisitors.h"
+#include <exception>
+
 
 SemanticChecker::SemanticChecker()
 {
@@ -162,29 +164,111 @@ void SemanticChecker::visit(Selection * node)
 
 void SemanticChecker::visit(Join * node)
 {
+	std::map<std::string,ColumnInfo> outputColumns0,outputColumns1;
 	node->leftChild->accept(*this);
+	outputColumns0=outputColumns;
 	node->rightChild->accept(*this);
+	outputColumns1=outputColumns;
+
+	if(node->condition!=0)
+	{
+		std::shared_ptr<SemanticExpressionVisitor> expresionVisitor;
+		expresionVisitor=std::shared_ptr<SemanticExpressionVisitor>(new SemanticExpressionVisitor());
+		expresionVisitor->outputColumns0=outputColumns0;
+		expresionVisitor->outputColumns1=outputColumns1;
+		node->condition->accept(*expresionVisitor);
+		containsErrors|=expresionVisitor->containsErrors;
+	}
+	outputColumns.clear();
 	for(auto it=node->outputColumns.begin();it!=node->outputColumns.end();++it)
 	{
-		outputColumns[it->newName]=ColumnInfo(it->newName,"");
+		if(outputColumns.find(it->newName)==outputColumns.end())
+		{
+			outputColumns[it->newName]=ColumnInfo(it->newName,"");
+		}
+		else
+		{
+			ReportError("Columns must have different name"); 
+		}
 	}
 }
 
 void SemanticChecker::visit(AntiJoin * node)
 {
+	std::map<std::string,ColumnInfo> outputColumns0,outputColumns1;
 	node->leftChild->accept(*this);
+	outputColumns0=outputColumns;
 	node->rightChild->accept(*this);
+	outputColumns1=outputColumns;
+
+	std::shared_ptr<SemanticExpressionVisitor> expresionVisitor;
+	expresionVisitor=std::shared_ptr<SemanticExpressionVisitor>(new SemanticExpressionVisitor());
+	expresionVisitor->outputColumns0=outputColumns0;
+	expresionVisitor->outputColumns1=outputColumns1;
+	node->condition->accept(*expresionVisitor);
+	containsErrors|=expresionVisitor->containsErrors;
+
 	outputColumns.clear();
 	for(auto it=node->outputColumns.begin();it!=node->outputColumns.end();++it)
 	{
-		outputColumns[it->newName]=ColumnInfo(it->newName,"");
+		if(outputColumns.find(it->newName)==outputColumns.end())
+		{
+			if(it->input==0)
+			{
+				if(outputColumns0.find(it->newName)==outputColumns0.end())
+				{
+					ReportError("Column not found in input 0"); 
+				}
+				else
+				{
+					outputColumns[it->newName]=ColumnInfo(it->newName,"");
+				}
+			}
+			else if(it->input==1)
+			{
+				if(outputColumns1.find(it->newName)==outputColumns1.end())
+				{
+					ReportError("Column not found in input 1"); 
+				}
+				else
+				{
+					outputColumns[it->newName]=ColumnInfo(it->newName,"");
+				}
+			}
+			else
+			{
+				throw new std::exception("Error");
+			}
+
+			
+		}
+		else
+		{
+			ReportError("Columns must have different name"); 
+		}
 	}
 }
 	
 void SemanticChecker::visit(Union * node)
 {
+	std::map<std::string,ColumnInfo> outputColumns0,outputColumns1;
 	node->leftChild->accept(*this);
+	outputColumns0=outputColumns;
 	node->rightChild->accept(*this);
+	outputColumns1=outputColumns;
+	
+	if(outputColumns0.size()!=outputColumns1.size())
+	{
+		ReportError("Union requires inputs to have same size"); 
+	}
+
+	for(auto it=outputColumns0.begin();it!=outputColumns0.end();++it)
+	{
+		if(outputColumns1.find(it->first)==outputColumns1.end())
+		{
+			ReportError("Union requires inputs to have same columns"); 
+		}	
+	}
 }
 
 void SemanticChecker::visit(GroupedJoin * node)
